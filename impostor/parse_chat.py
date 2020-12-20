2fafab78886ad46f67fa29bff7823ffc415b44dd
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2020-12-20 16:16:59 +0700

    Changed parse_chat output back to List[List[Tuple[bool, str]]]
    List of conversations
    Each conversation is a list of utterances
    Each utterance is tuple (is_user, utterance)

--- impostor/parse_chat.py
+++ impostor/parse_chat.py
@@ -4,7 +4,7 @@ import yaml
 
 import re
 
-config = yaml.safe_load(open("../config.yaml"))
+config = yaml.safe_load(open("config.yaml"))
 
 user_name = config["user-name"]
 
@@ -23,8 +23,7 @@ Also converts newlines into <lsep>.
 """
 
 
-def parse_chat_logs(file_path: str) -> List[Tuple[bool, List[str]]]:
-    words = []
+def parse_chat_logs(file_path: str) -> List[List[Tuple[bool, str]]]:
     with open(file_path, "r") as f:
         chats = f.read()
     chats = chats.split("\n")
@@ -39,23 +38,23 @@ def parse_chat_logs(file_path: str) -> List[Tuple[bool, List[str]]]:
             continue
         if re.fullmatch(date_regex, line):
             if len(current_convo) > 0:
-                merged_chats.append((sender == user_name, current_convo))
+                merged_chats.append(current_convo)
             current_convo = []
         elif m := re.fullmatch(new_chat_regex, line):
             if len(current_msg) > 0:
-                current_convo.append(current_msg)
+                current_convo.append((sender == user_name, current_msg))
             current_msg = m.groups()[2]
             sender = m.groups()[1]
         else:
             current_msg += " <lsep> " + line
 
     if len(current_convo) > 0:
-        current_convo.append(current_msg)
+        current_convo.append((sender == user_name, current_msg))
     if len(current_msg) > 0:
-        merged_chats.append((sender == user_name, current_convo))
+        merged_chats.append(current_convo)
 
     return merged_chats
 
 
 if __name__ == "__main__":
-    print(parse_chat_logs("../data/test.txt"))
+    print(parse_chat_logs("../data/raw-history-test/test.txt"))
