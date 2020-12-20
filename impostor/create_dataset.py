ded3a1eb307fdd3aedd6f3781403a8541f796a98
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2020-12-20 19:59:39 +0700

    Refractored create_dataset into a function

--- impostor/create_dataset.py
+++ impostor/create_dataset.py
@@ -14,21 +14,15 @@ from impostor.parse_chat import parse_chat_logs
 
 config = yaml.safe_load(open("config.yaml"))
 
-INPUT_DIR = config["chat_log_directory"]
-OUTPUT_FILE = config["dataset"]["file"]
-NUM_CANDIDATES = config["dataset"]["num_candidates"]
-MAX_HISTORY = config["dataset"]["max_history"]
 
-if __name__ == "__main__":
-    print(INPUT_DIR, OUTPUT_FILE, NUM_CANDIDATES)
-
-    files_to_parse = os.listdir(INPUT_DIR)
+def create_dataset(input_dir: str, output_file: str, num_candidates: int, max_history: int):
+    files_to_parse = os.listdir(input_dir)
 
     dataset = defaultdict(list)
 
     parsed_logs = []
     for file in files_to_parse:
-        parsed = parse_chat_logs(os.path.join(INPUT_DIR, file))
+        parsed = parse_chat_logs(os.path.join(input_dir, file))
         parsed_logs.append(parsed)
 
     utterances = set()
@@ -42,17 +36,26 @@ if __name__ == "__main__":
     for parsed in parsed_logs:
         for dialog in parsed:
             for i, utterance in enumerate(dialog):
-                history = dialog[max(0, i-MAX_HISTORY):i]
+                history = dialog[max(0, i - max_history):i]
                 # replies = utterance + np.random.choice(utterances, NUM_CANDIDATES - 1)
                 candidates = [utterance]
-                for _ in range(NUM_CANDIDATES - 1):
+                for _ in range(num_candidates - 1):
                     while (x := utterances[np.random.randint(0, len(utterances))]) in candidates:
                         pass
                     candidates.append(x)
-                correct = np.random.randint(0, NUM_CANDIDATES)
+                correct = np.random.randint(0, num_candidates)
                 candidates[correct], candidates[0] = candidates[0], candidates[correct]
                 dataset["history"].append(history)
                 dataset["candidates"].append(candidates)
                 dataset["correct"].append(correct)
 
-    torch.save(dataset, OUTPUT_FILE)
+    torch.save(dataset, output_file)
+
+
+if __name__ == "__main__":
+    input_dir = config["chat_log_directory"]
+    output_file = config["dataset"]["file"]
+    max_candidates = config["dataset"]["num_candidates"]
+    max_history = config["dataset"]["max_history"]
+    print(input_dir, output_file, max_candidates)
+    create_dataset(input_dir, output_file, max_candidates, max_history)
