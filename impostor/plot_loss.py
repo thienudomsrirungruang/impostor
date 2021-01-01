543437846b8a073020d877a5621b499f9f44e571
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2021-01-01 18:06:27 +0700

    Updated graph plotter to use subplots and plot more things

--- impostor/plot_loss.py
+++ impostor/plot_loss.py
@@ -4,26 +4,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def moving_avg(x, y, num_avg: int):
+    xnew = np.convolve(x, np.ones(num_avg), "valid") / num_avg
+    ynew = np.convolve(y, np.ones(num_avg), "valid") / num_avg
+    return xnew, ynew
+
+
 def plot_loss_graph(path_to_file: str):
-    data = []  # iterations, epochs, losses, lm_losses, mc_losses
+    train_data = []  # iterations, epochs, losses, lm_losses, mc_losses
+    test_data = []  # iteration, epoch, mc_correct, num_tests, lm_correct, lm_tested
     with open(path_to_file, "r") as f:
         for line in f:
-            data.append(list(map(float, line.strip().split(","))))
+            split_line = line.strip().split(",")
+            if split_line[0] == "train":
+                train_data.append(list(map(float, split_line[1:])))
+            elif split_line[0] == "test":
+                test_data.append(list(map(int, split_line[1:])))
 
-    data = np.array(data)
+    train_data = np.array(train_data)
+    test_data = np.array(test_data)
 
-    fig, ax = plt.subplots()
+    mc_accuracy = test_data[:, 2] / test_data[:, 3]
+    lm_accuracy = test_data[:, 4] / test_data[:, 5]
 
-    ax.set_yscale("log")
+    fig, ax = plt.subplots(2, 2)
 
-    ax.scatter(data[:, 0], data[:, 2], marker=".", color="blue", alpha=0.01)
-    ax.scatter(data[:, 0], data[:, 3], marker=".", color="red", alpha=0.01)
-    ax.scatter(data[:, 0], data[:, 4], marker=".", color="green", alpha=0.01)
-
-    print(data)
+    ax[0][0].scatter(train_data[:, 0], train_data[:, 2], marker=".", color="blue", alpha=0.01)
+    ax[0][0].plot(*moving_avg(train_data[:, 0], train_data[:, 2], 1000), color="black")
+    ax[0][0].set_title("Reconstruction loss")
+    ax[0][0].set_yscale("log")
+    ax[0][1].scatter(train_data[:, 0], train_data[:, 3], marker=".", color="red", alpha=0.01)
+    ax[0][1].plot(*moving_avg(train_data[:, 0], train_data[:, 3], 1000), color="black")
+    ax[0][1].set_title("Sequence generation loss")
+    ax[0][1].set_yscale("log")
+    ax[1][0].scatter(train_data[:, 0], train_data[:, 4], marker=".", color="green", alpha=0.01)
+    ax[1][0].plot(*moving_avg(train_data[:, 0], train_data[:, 4], 1000), color="black")
+    ax[1][0].set_title("Sequence prediction loss")
+    ax[1][0].set_yscale("log")
+    ax[1][1].scatter(test_data[:, 0], mc_accuracy, color="green")
+    ax[1][1].scatter(test_data[:, 0], lm_accuracy, color="red")
+    ax[1][1].set_ylim(0, 1)
 
     plt.show()
 
 
 if __name__ == "__main__":
-    plot_loss_graph("log/log-20-12-22-13-00-17.txt")
+    plot_loss_graph("log/log-21-01-01-07-44-37.txt")
