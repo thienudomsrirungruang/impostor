3ceef3fd5bc41a37b726bbce86f01df2afc69292
Thien Udomsrirungruang <thien.udomsrirungruang@gmail.com>
2021-01-01 13:32:51 +0700

    Added special tokens and detection of calls, photos etc.

--- impostor/parse_chat.py
+++ impostor/parse_chat.py
@@ -5,6 +5,8 @@ import yaml
 
 import re
 
+from special_tokens import photo, video, sticker, voice, call
+
 config = yaml.safe_load(open(os.path.join(os.path.dirname(__file__), "config.yaml")))
 
 user_name = config["user-name"]
@@ -12,6 +14,22 @@ user_name = config["user-name"]
 new_chat_regex = r"(\d{2}:\d{2})\t([^\t]*)\t(.*)"
 
 date_regex = r"(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun), \d{2}\/\d{2}\/\d{4}"
+
+
+def preprocess_message(msg: str) -> str:
+    """Tokenizes photos/videos/stickers/voice messages/calls"""
+    if msg == "[Photo]":
+        return photo
+    if msg == "[Video]":
+        return video
+    if msg == "[Sticker]":
+        return sticker
+    if msg == "[Voice message]":
+        return voice
+    if re.match(r"^Missed|Cancelled call|Call time (\d{1,2}:)?\d{1,2}:\d{2}$", msg):
+        return call
+    return msg
+
 
 
 def parse_chat_logs(file_path: str) -> List[List[Tuple[bool, str]]]:
@@ -42,14 +60,14 @@ def parse_chat_logs(file_path: str) -> List[List[Tuple[bool, str]]]:
             current_convo = []
         elif m:
             if len(current_msg) > 0:
-                current_convo.append((sender == user_name, current_msg))
+                current_convo.append((sender == user_name, preprocess_message(current_msg)))
             current_msg = m.groups()[2]
             sender = m.groups()[1]
         else:
             current_msg += " <lsep> " + line
 
     if len(current_convo) > 0:
-        current_convo.append((sender == user_name, current_msg))
+        current_convo.append((sender == user_name, preprocess_message(current_msg)))
     if len(current_msg) > 0:
         merged_chats.append(current_convo)
 
--- impostor/special_tokens.py
+++ impostor/special_tokens.py
@@ -1,4 +1,7 @@
 bos, eos, speaker_self, speaker_other, lsep, pad = "<bos>", "<eos>", "<speaker_self>", "<speaker_other>", "<lsep>", "<pad>"
 
+photo, call, video, voice, sticker = "<photo>", "<call>", "<video>", "<voice>", "<sticker>"
+
 SPECIAL_TOKENS = {"bos_token": bos, "eos_token": eos,
-                  "additional_special_tokens": [speaker_self, speaker_other, lsep], "pad_token": pad}
+                  "additional_special_tokens": [speaker_self, speaker_other, lsep, photo, call, video, voice, sticker],
+                  "pad_token": pad}
